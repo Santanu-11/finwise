e410636166f692eb982a02a3a9f369a4a28373ec
Santanu-11 <singhsantanu683@gmail.com>
2026-05-28 15:24:08 +0530

    Working Well

--- app/api/debug/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    uri: process.env.MONGODB_URI,
-    deployId: process.env.VERCEL_DEPLOYMENT_ID,
-  });
-}
